4d24bc049e23ffd0a199d9041ed5e74e54b62e19
is319 <is319@ic.ac.uk>
2020-01-21 11:37:59 +0000

    as3

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -8,10 +8,11 @@ public class QueryProcessor {
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist;";
         }
-        if (query.toLowerCase().contains("which of the following numbers is the largest")) {
-            String numbers = query.toLowerCase().split(":")[1];
+        if (query.toLowerCase().contains("largest")) {
+            String numbers = query.toLowerCase().split(": ")[2];
             Integer n1 = Integer.parseInt(numbers.split(", ")[0]);
             Integer n2 = Integer.parseInt(numbers.split(", ")[1]);
+
             return String.valueOf(Math.max(n1, n2));
         }
         return "";
