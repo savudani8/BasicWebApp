c5a42b7f0f22612bf1e646ff89059bd5dc1128f4
is319 <is319@ic.ac.uk>
2020-01-21 11:23:03 +0000

    as

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -8,6 +8,9 @@ public class QueryProcessor {
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist;";
         }
+        if (query.toLowerCase().contains("what is your team name")) {
+            return "Duncanny Valley";
+        }
         return "";
     }
 }
